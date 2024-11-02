9b5b81d46cf725ee03cff21585cb80f05b581a89
Arghya Sadhukhan <connect@adben.in>
2024-11-02 15:17:03 +0530

    renamed bootloader project

---
+++ bootloader/firmware/usb_bootloader.X/build/usbdevice_pic32mz_ef_sk/debug/_ext/1360937237/main.o.d
@@ -0,0 +1,7 @@
+build/usbdevice_pic32mz_ef_sk/debug/_ext/1360937237/main.o: ../src/main.c \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h:
---
+++ bootloader/firmware/usb_bootloader.X/build/usbdevice_pic32mz_ef_sk/debug/_ext/612711379/sys_reset.o.d
@@ -0,0 +1,110 @@
+build/usbdevice_pic32mz_ef_sk/debug/_ext/612711379/sys_reset.o: \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/reset/src/sys_reset.c \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/reset/sys_reset.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/system.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/plib_reset.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_processor.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_p32xxxx.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_p32mz2048efm144.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/peripheral_common_32bit.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/peripheral_common.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_ResetReasonStatus_MZ.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_SoftwareResetTrigger_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_ConfigRegReadError_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_NmiControl_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_WdtoInSleep_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_NmiCounter_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/devcon/sys_devcon.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/plib_devcon.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_processor.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_p32xxxx.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_p32mz2048efm144.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_AlternateClock_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_DeviceRegsLockUnlock_PIC32MZ.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_OTPConfigLockUnlock_Unsupported.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_USB_PHY_SleepModeSet_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_ECCModes_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_JTAGEnable_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_AnalogChargePumpControl_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_TraceOutput_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_JTAGUsesTDO_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_IgnoreDebugFreeze_Unsupported.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_DeviceVerAndId_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_SysLockUnlock_Default.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_MPLL_Unsupported.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_HSUARTControl_Unsupported.h \
+ ../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_BootSelection_Unsupported.h
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/reset/sys_reset.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/system.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/plib_reset.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_processor.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_p32xxxx.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/reset_p32mz2048efm144.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/peripheral_common_32bit.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/peripheral_common.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_ResetReasonStatus_MZ.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_SoftwareResetTrigger_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_ConfigRegReadError_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_NmiControl_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_WdtoInSleep_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/reset/processor/../templates/reset_NmiCounter_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/devcon/sys_devcon.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/plib_devcon.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_processor.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_p32xxxx.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/devcon_p32mz2048efm144.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_AlternateClock_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_DeviceRegsLockUnlock_PIC32MZ.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_OTPConfigLockUnlock_Unsupported.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_USB_PHY_SleepModeSet_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_ECCModes_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_JTAGEnable_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_AnalogChargePumpControl_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_TraceOutput_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_JTAGUsesTDO_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_IgnoreDebugFreeze_Unsupported.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_DeviceVerAndId_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_SysLockUnlock_Default.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_MPLL_Unsupported.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_HSUARTControl_Unsupported.h:
+
+../src/system_config/usbdevice_pic32mz_ef_sk/framework/peripheral/devcon/processor/../templates/devcon_BootSelection_Unsupported.h:
