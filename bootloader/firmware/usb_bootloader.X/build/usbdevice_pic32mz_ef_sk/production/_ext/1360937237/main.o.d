ad259a34e80746d905110461911271c3b00c3f72
Arghya Sadhukhan <connect@adben.in>
2024-11-24 20:59:19 +0530

    modified gitignore

--- bootloader/firmware/usb_bootloader.X/build/usbdevice_pic32mz_ef_sk/production/_ext/1360937237/main.o.d
+++
@@ -1,8 +0,0 @@
-build/usbdevice_pic32mz_ef_sk/production/_ext/1360937237/main.o: \
- ../src/main.c \
- ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h \
- ../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h
-
-../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_module.h:
-
-../src/system_config/usbdevice_pic32mz_ef_sk/framework/system/common/sys_common.h:
