27e217186fd5c75d2f71843c243d2fa2be664ba4
Christopher Lange <clange@daqifi.com>
2025-07-17 18:31:57 -0600

    modified gethipinfo to require ? since it is a query

--- firmware/src/services/SCPI/SCPIInterface.c
+++ firmware/src/services/SCPI/SCPIInterface.c
@@ -776,7 +776,7 @@ static const scpi_command_t scpi_commands[] = {
     {.pattern = "SYSTem:COMMunicate:LAN:FACRESET", .callback = SCPI_LANSettingsFactoryLoad,},
     {.pattern = "SYSTem:COMMunicate:LAN:CLEAR", .callback = SCPI_LANSettingsClear,},
     //
-    {.pattern = "SYSTem:COMMunicate:LAN:GETChipInfo", .callback = SCPI_LANGetChipInfo,},
+    {.pattern = "SYSTem:COMMunicate:LAN:GETChipInfo?", .callback = SCPI_LANGetChipInfo,},
     // ADC
     {.pattern = "MEASure:VOLTage:DC?", .callback = SCPI_ADCVoltageGet,},
     {.pattern = "ENAble:VOLTage:DC", .callback = SCPI_ADCChanEnableSet,},
